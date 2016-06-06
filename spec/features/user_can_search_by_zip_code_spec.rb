b8763b9f544665892890c63c84300cb8284fb825
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-06 10:22:48 -0600

    first test finalized with distance

--- spec/features/user_can_search_by_zip_code_spec.rb
+++ spec/features/user_can_search_by_zip_code_spec.rb
@@ -10,16 +10,8 @@ RSpec.describe "user can enter in a zip code" do
     within("#UDR") do
       expect(page).to have_content("800 Acoma St")
       expect(page).to have_content("ELEC")
+      expect(page).to have_content("Miles Away: 0.3117")
       expect(page).to have_content("24 hours daily")
     end
   end
 end
-
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203
-# And I click "Locate"
-# Then I should be on page "/search?zip=80203"
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
